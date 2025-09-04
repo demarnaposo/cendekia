e6dd38ce102cc0d54a263eed947781233624a5aa
Waldemar Naposo <waldemarnaposo313@gmail.com>
2025-09-04 16:37:25 +0700

    Add App Layout and fix

---
+++ resources/js/Pages/Testing.jsx
@@ -0,0 +1,5 @@
+import AppLayout from '@/Layouts/AppLayout';
+
+export default function Testing() {
+    return <AppLayout title="Testing">ini adalah testing untuk layput</AppLayout>;
+}
--- resources/js/lib/utils.js
+++ resources/js/lib/utils.js
@@ -1,14 +1,14 @@
-import { clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
 
 export function cn(...inputs) {
     return twMerge(clsx(inputs));
 }
 
 export const FINEPAYMENTSTATUS = {
-    PENDING: "TERTUNDA",
-    SUCCESS: "SUKSES",
-    FAILED: "GAGAL",
+    PENDING: 'TERTUNDA',
+    SUCCESS: 'SUKSES',
+    FAILED: 'GAGAL',
 };
 
 export function flashMessage(params) {
@@ -16,9 +16,9 @@ export function flashMessage(params) {
 }
 
 export const formatToRupiah = (amount) => {
-    const formatter = new Intl.NumberFormat("id-ID", {
-        style: "currency",
-        currency: "IDR",
+    const formatter = new Intl.NumberFormat('id-ID', {
+        style: 'currency',
+        currency: 'IDR',
         minimumFractionDigits: 0,
         maximumFractionDigits: 0,
     });
@@ -28,34 +28,33 @@ export const formatToRupiah = (amount) => {
 
 export const messages = {
     503: {
-        title: "Service Unavailable",
-        description:
-            "Sorry, we are doing some maintenance. Please check back soon.",
-        status: "503",
+        title: 'Service Unavailable',
+        description: 'Sorry, we are doing some maintenance. Please check back soon.',
+        status: '503',
     },
     500: {
-        title: "Service Error",
-        description: "Oops, something went wrong on our servers.",
-        status: "500",
+        title: 'Service Error',
+        description: 'Oops, something went wrong on our servers.',
+        status: '500',
     },
     404: {
-        title: "Not Found",
-        description: "Sorry, the page you are looking for could not be found.",
-        status: "404",
+        title: 'Not Found',
+        description: 'Sorry, the page you are looking for could not be found.',
+        status: '404',
     },
     403: {
-        title: "Forbidden",
-        description: "Sorry, you do not have permission to access this page.",
-        status: "403",
+        title: 'Forbidden',
+        description: 'Sorry, you do not have permission to access this page.',
+        status: '403',
     },
     401: {
-        title: "Unauthorized",
-        description: "Sorry, you are not authorized to access this page.",
-        status: "401",
+        title: 'Unauthorized',
+        description: 'Sorry, you are not authorized to access this page.',
+        status: '401',
     },
     429: {
-        title: "Too Many Requests",
-        description: "Please try again in just a second.",
-        status: "429",
+        title: 'Too Many Requests',
+        description: 'Please try again in just a second.',
+        status: '429',
     },
 };
